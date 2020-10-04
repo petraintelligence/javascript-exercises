675a319be700e30c320660902c5f573c6663470d
petraintelligence <brown.joshua.scott@gmail.com>
2020-10-04 07:01:36 -0500

    repeatString passing test cases 1-5

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,6 +1,9 @@
 const repeatString = function(string, repeatCount) {
-    let concatString = string;
-    for(i = 1; i < repeatCount; i++) {
+    let concatString = '';
+    if (repeatCount < 0){
+        return 'ERROR';
+    }
+    for(i = 0; i < repeatCount; i++) {
         concatString += string;
     }
     return concatString;
