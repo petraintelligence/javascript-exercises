642e7a0e343d16a6dc9d520d5651414a553655ca
petraintelligence <brown.joshua.scott@gmail.com>
2020-10-04 06:58:11 -0500

    repeat string test case 1 passed

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatCount) {
+    let concatString = string;
+    for(i = 1; i < repeatCount; i++) {
+        concatString += string;
+    }
+    return concatString;
 }
 
 module.exports = repeatString
