b13d2e0276972c0b682c8963f5c8805540d88f5c
petraintelligence <brown.joshua.scott@gmail.com>
2020-10-05 08:31:00 -0500

    removefromArray passed test cases

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    
+    const array = args[0]
+    return array.filter(value => !args.includes(value));
 }
 
 module.exports = removeFromArray
